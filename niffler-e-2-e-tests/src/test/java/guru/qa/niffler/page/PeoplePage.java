5481523deb5a6ca555d435266369ea978b1a5a66
Alexey <alexey.melnik@icloud.com>
2024-04-30 01:22:13 -0300

    refactor code

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/PeoplePage.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/PeoplePage.java
@@ -9,31 +9,31 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class PeoplePage {
     final ElementsCollection
-            rows = $(".abstract-table tbody").
-            $$("tr");
+            rows = $(".abstract-table tbody")
+            .$$("tr");
 
     public SelenideElement userNameRow (String username) {
         return rows.find((text(username)));
     }
 
     public boolean isFriend (String username){
-        userNameRow(username).lastChild().
-                $("div").
-                shouldHave(text("You are friends"));
+        userNameRow(username).lastChild()
+                .$("div")
+                .shouldHave(text("You are friends"));
         return true;
     }
 
     public boolean isInviteSent (String username){
-        userNameRow(username).lastChild().
-                $("div").
-                shouldHave(text("Pending invitation"));
+        userNameRow(username).lastChild()
+                .$("div")
+                .shouldHave(text("Pending invitation"));
         return true;
     }
 
     public boolean isInvitationReceived(String username) {
         userNameRow(username).lastChild().
-                $(".abstract-table__buttons div").
-                shouldHave(attribute(
+                $(".abstract-table__buttons div")
+                .shouldHave(attribute(
                         "data-tooltip-id",
                         "submit-invitation"));
         return true;
